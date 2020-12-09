c85f910dc869356211e16a6a28ae01914dcab921
Diego <dgentner@ucol.mx>
2020-12-09 01:53:02 -0600

    Added first function

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-export default class App {}
+export default class App {
+
+    sumarParesFor() {
+        let resultado,i;
+        resultado=0;
+    for (i = 1; i <= 20; i++) {
+       if ((i % 2)==0) {
+        resultado = resultado + i;   
+       }
+        
+    }
+    return resultado;
+}
+
+
+
+} // No Borrar - No Borrar
+
 
 let app = new App();
-dsfsdfsdf
+console.log(app.sumarParesFor())
+console.log(app.contarImparesFor(1,20))
+console.log(app.sumarParesWhile())
+console.log(app.contarImparesWhile())
